35936ae9b7f9d9f35988a199a2648320b87b9760
Joaquin Guaglianone <Joaquinguaglianone@hotmail.hotmail.com>
2023-09-26 15:54:52 -0300

    Entregable Documentacion

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
-import handlebars from "express-handlebars"
+import handlebars from "express-handlebars";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import MongoStore from "connect-mongo";
 import passport from "passport";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUIExpress from "swagger-ui-express"
 import { Server } from "socket.io";
 import __dirname from "./util.js";
 import { proRoute } from "./routes/productsRoute.js";
@@ -13,86 +15,104 @@ import { cartRoute } from "./routes/CartRoute.js";
 import { messagesRoute } from "./routes/messagesRoute.js";
 import { routerView } from "./routes/viewsRouter.js";
 import routerUser from "./routes/userRouter.js";
-import initializePassport from './config/passport.config.js';
+import initializePassport from "./config/passport.config.js";
 import { addLogger, loggerInfo } from "./logger/logger.js";
 
-
-dotenv.config()
+dotenv.config();
 
 const app = express();
 app.use(addLogger);
 
-const serverHttp = app.listen(process.env.ServerPort, () =>  
-{const info = loggerInfo()  
-  info.info(`The server is working correctly on the port ${process.env.ServerPort}`)
-  ;}
-  )
+const serverHttp = app.listen(process.env.ServerPort, () => {
+  const info = loggerInfo();
+  info.info(
+    `The server is working correctly on the port ${process.env.ServerPort}`
+  );
+});
 
-const MONGO = `mongodb+srv://joaquinGuaglianone:${process.env.superSecret}@back-end-cluster.kle3ie9.mongodb.net/ecommerce`
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.1",
+
+    info: {
+      title: "Documentacion API Gaming Center",
+
+      description: "Documentacion api de docs",
+    },
+  },
+
+  apis: [`${__dirname}/docs/*.yaml`],
+};
+
+const MONGO = `mongodb+srv://joaquinGuaglianone:${process.env.superSecret}@back-end-cluster.kle3ie9.mongodb.net/ecommerce`;
 const connection = mongoose.connect(MONGO, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true,
+});
 
-app.use(express.json())
+app.use(express.json());
 
-export const socketServer = new Server(serverHttp)
 
-app.use(express.urlencoded({extended: true}))
+const specs = swaggerJSDoc(swaggerOptions);
 
-app.engine('handlebars', handlebars.engine())
-app.set('views', __dirname + '/views');
-app.set('view engine', 'handlebars')
-app.use(cookieParser("coderhouse"))
-app.use(session({
-  store: new MongoStore({
-    mongoUrl: MONGO,
-    ttl: 3600
-}),
+export const socketServer = new Server(serverHttp);
 
-  secret: " coderhouse ",
-  resave : " false ",
-  saveUninitialize : " false "
-}))
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+app.use(cookieParser("coderhouse"));
+app.use(
+  session({
+    store: new MongoStore({
+      mongoUrl: MONGO,
+      ttl: 3600,
+    }),
+
+    secret: " coderhouse ",
+    resave: " false ",
+    saveUninitialize: " false ",
+  })
+);
 
 initializePassport();
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(__dirname + '/public'));
-app.use("/", routerView)
+app.use(express.static(__dirname + "/public"));
+app.use("/", routerView);
 app.use("/api/products", proRoute);
-app.use("/api/cart", cartRoute)
-app.use("/api/messages", messagesRoute)
-app.use('/api/sessions', routerUser);
-
-
+app.use("/api/cart", cartRoute);
+app.use("/api/messages", messagesRoute);
+app.use("/api/sessions", routerUser);
+app.use('/apidocs', swaggerUIExpress.serve, swaggerUIExpress.setup(specs));
 
 app.get("/api/loggerTest", (req, res) => {
-  req.logger.debug('Error debug Dev')
-  req.logger.http("HTTP Error Dev")
-  req.logger.info("Info Dev")
-  req.logger.warning("Warning Dev")
-   req.logger.error("Un error")
+  req.logger.debug("Error debug Dev");
+  req.logger.http("HTTP Error Dev");
+  req.logger.info("Info Dev");
+  req.logger.warning("Warning Dev");
+  req.logger.error("Un error");
   // req.logger.fatal("Un error fatal")
-  res.send('')
-})
+  res.send("");
+});
 
-app.get("/session", (req,res) => {
+app.get("/session", (req, res) => {
   if (!req.session.count) {
     res.send("bienvenido a la pagina");
     return;
   }
 
-  req.session.count ++
-  res.send(`usted a ingresado a la pagina ${req.session.count} veces`)
-
-})
+  req.session.count++;
+  res.send(`usted a ingresado a la pagina ${req.session.count} veces`);
+});
 
 socketServer.on("connection", (socket) => {
   console.log("Usuario conectado", socket.id);
 
-  socket.on('message', (data) => {
-    console.log(data)
-  })
+  socket.on("message", (data) => {
+    console.log(data);
+  });
 });
